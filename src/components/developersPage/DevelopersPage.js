6ce2f83e34110a63f48a9cbf38862d343667f407
MadaShindeInai <1992@sergei.by>
2020-02-26 23:33:27 +0300

    feat: add photos and links to github in disighers page

--- src/components/developersPage/DevelopersPage.js
+++ src/components/developersPage/DevelopersPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import Photo from '../../assets/images/bird.jpg';
+import PhotoDaria from '../../assets/images/dariavv.jpg';
+import Deeamon from '../../assets/images/deeamon.jpg';
+import Max from '../../assets/images/max.jpg';
 import './DevelopersPage.scss';
 
 const DevelopersPageBE = ({ language }) => {
@@ -15,27 +17,27 @@ const DevelopersPageBE = ({ language }) => {
     titleText = 'Наша команда';
     firstDeveloperName = 'Дарья Сидорович';
     secondDeveloperName = 'Сергей Непряхин';
-    thirdDeveloperName = 'Анастасия ';
+    thirdDeveloperName = 'Анастасия Носевич';
     forthDeveloperName = 'Максим Деменков';
-    fifthDeveloperName = 'Марина ';
+    fifthDeveloperName = 'Марина Сугутова';
     linkToGithub = 'Ссылка на GitHub';
     linkToWorklog = 'Ссылка на Worklog';
   } else if (language === 'EN') {
     titleText = 'Our team';
     firstDeveloperName = 'Darya Sidarovich';
     secondDeveloperName = 'Siarhei Niaprakhin';
-    thirdDeveloperName = 'Anastasya';
+    thirdDeveloperName = 'Anastasya Nasevich';
     forthDeveloperName = 'Maxim Demenkov';
-    fifthDeveloperName = 'Maryna';
+    fifthDeveloperName = 'Marina Sugutova';
     linkToGithub = 'Link to GitHub';
     linkToWorklog = 'Link to Worklog';
   } else if (language === 'BE') {
     titleText = 'Наша каманда';
     firstDeveloperName = 'Дар`я Сідаровіч';
     secondDeveloperName = 'Сяргей Няпрахiн';
-    thirdDeveloperName = 'Анастасiя';
+    thirdDeveloperName = 'Анастасiя Насевіч';
     forthDeveloperName = 'Максiм Дземянкоў';
-    fifthDeveloperName = 'Марына';
+    fifthDeveloperName = 'Марына Сугутава';
     linkToGithub = 'Спасылка на GitHub';
     linkToWorklog = 'Спасылка на Worklog';
   }
@@ -45,18 +47,18 @@ const DevelopersPageBE = ({ language }) => {
       <div className="developers">
         <div className="developer-container">
           <img
-            src={Photo}
+            src={PhotoDaria}
             className="developer-container__photo"
             alt="pic"
             width="190"
           />
           <p className="developer-container__name">{firstDeveloperName}</p>
-          <a href="https://github.com" target="_blank" rel='noreferrer noopener'>{linkToGithub}</a>
+          <a href="https://github.com/dariavv" target="_blank" rel='noreferrer noopener'>{linkToGithub}</a>
           <a href="https://github.com" target="_blank" rel='noreferrer noopener'>{linkToWorklog}</a>
         </div>
         <div className="developer-container">
           <img
-            src={Photo}
+            src='https://sun2.cosmostv-by-minsk.userapi.com/c845219/v845219353/1b0e9c/nZ1HrrJ3aUc.jpg?ava=1'
             className="developer-container__photo"
             alt="pic"
             width="190"
@@ -67,35 +69,35 @@ const DevelopersPageBE = ({ language }) => {
         </div>
         <div className="developer-container">
           <img
-            src={Photo}
+            src='https://avatars1.githubusercontent.com/u/54587368?s=460&v=4'
             className="developer-container__photo"
             alt="pic"
             width="190"
           />
           <p className="developer-container__name">{thirdDeveloperName}</p>
-          <a href="https://github.com" target="_blank" rel='noreferrer noopener'>{linkToGithub}</a>
+          <a href="https://github.com/Anastasya1701" target="_blank" rel='noreferrer noopener'>{linkToGithub}</a>
           <a href="https://github.com" target="_blank" rel='noreferrer noopener'>{linkToWorklog}</a>
         </div>
         <div className="developer-container">
           <img
-            src={Photo}
+            src={Max}
             className="developer-container__photo"
             alt="pic"
             width="190"
           />
           <p className="developer-container__name">{forthDeveloperName}</p>
-          <a href="https://github.com" target="_blank" rel='noreferrer noopener'>{linkToGithub}</a>
+          <a href="https://github.com/MaxDeMaW" target="_blank" rel='noreferrer noopener'>{linkToGithub}</a>
           <a href="https://github.com" target="_blank" rel='noreferrer noopener'>{linkToWorklog}</a>
         </div>
         <div className="developer-container">
           <img
-            src={Photo}
+            src={Deeamon}
             className="developer-container__photo"
             alt="pic"
             width="190"
           />
           <p className="developer-container__name">{fifthDeveloperName}</p>
-          <a href="https://github.com" target="_blank" rel='noreferrer noopener'>{linkToGithub}</a>
+          <a href="https://github.com/Deeamon" target="_blank" rel='noreferrer noopener'>{linkToGithub}</a>
           <a href="https://github.com" target="_blank" rel='noreferrer noopener'>{linkToWorklog}</a>
         </div>
 
