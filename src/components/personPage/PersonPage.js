b521b2a3f7520a543fd8979891ca5a6a0bd910c4
Darya Sidarovich <daria.sidorovich333@gmail.com>
2020-02-26 18:10:32 +0300

    feat: implement data from architectsData

--- src/components/personPage/PersonPage.js
+++ src/components/personPage/PersonPage.js
@@ -5,32 +5,49 @@ import './PersonPage.scss';
 
 const PersonPage = ({ language, id }) => {
   const currentAuthor = Architects.filter((person) => person.path === id);
-  console.log(currentAuthor);
+  const currentAuthorObj = currentAuthor[0];
 
   let name;
-  let authorPhoto;
-  let YearsOflife;
-  let ShortBiography;
+  let architectPhoto;
+  let date;
+  let description;
   if (language === 'RU') {
-    name = 'Вася';
-    YearsOflife = '1567г-1974г';
-    ShortBiography = 'Родился, жил, умер';
+    name = currentAuthorObj.nameRU;
+    architectPhoto = currentAuthorObj.img;
+    date = currentAuthorObj.date;
+    description = currentAuthorObj.descriptionRU;
   } else if (language === 'EN') {
-    name = 'Vasya';
-    YearsOflife = '1567-1974';
-    ShortBiography = 'Born, lived and died';
+    name = currentAuthorObj.nameEN;
+    architectPhoto = currentAuthorObj.img;
+    date = currentAuthorObj.date;
+    description = currentAuthorObj.descriptionEN;
   } else if (language === 'BE') {
-    name = 'Васiль';
-    YearsOflife = '1567г-1974г';
-    ShortBiography = 'Нарадзіўся, жыў і памёр';
+    name = currentAuthorObj.nameBY;
+    architectPhoto = currentAuthorObj.img;
+    date = currentAuthorObj.date;
+    description = currentAuthorObj.descriptionBY;
   }
   return (
-    <ul>
-      <li>{name}</li>
-      <li>{authorPhoto}</li>
-      <li>{YearsOflife}</li>
-      <li>{ShortBiography}</li>
-    </ul>
+    <div className="info-container">
+      <div className="info-container__item">
+        <h2 className="info-container__name">{name}</h2>
+      </div>
+      <div className="info-container__item">
+        <img className="info-container__image"
+          src={architectPhoto} alt="photo" width="250" />
+      </div>
+      <div className="info-container__item">
+        <h4 className="info-container__date">{date}</h4>
+      </div>
+      <div className="info-container__item">
+        <p className="info-container__description">{description}</p>
+      </div>
+      <div className="info-container__item">Timeline</div>
+      <div className="info-container__item">Проекты</div>
+      <div className="info-container__item">Галерея или Слайдер</div>
+      <div className="info-container__item">Видео</div>
+      <div className="info-container__item">Карта</div>
+    </div>
   )
 }
 
