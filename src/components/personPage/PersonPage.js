1c3b9e58a4e716d5af93ba0f1f5ec3543089a22e
MadaShindeInai <1992@sergei.by>
2020-02-26 17:21:50 +0300

    feat: add authorOfTheDay markup and logic

--- src/components/personPage/PersonPage.js
+++ src/components/personPage/PersonPage.js
@@ -5,22 +5,23 @@ import './PersonPage.scss';
 
 const PersonPage = ({ language, id }) => {
   const currentAuthor = Architects.filter((person) => person.path === id);
-  console.log(currentAuthor);
+  const currentAuthorObj = currentAuthor[0];
 
+  console.log("TCL: PersonPage -> currentAuthorObj", currentAuthorObj);
   let name;
   let authorPhoto;
   let YearsOflife;
   let ShortBiography;
   if (language === 'RU') {
-    name = 'Вася';
+    name = currentAuthorObj.nameRU;
     YearsOflife = '1567г-1974г';
     ShortBiography = 'Родился, жил, умер';
   } else if (language === 'EN') {
-    name = 'Vasya';
+    name = currentAuthorObj.nameEN;;
     YearsOflife = '1567-1974';
     ShortBiography = 'Born, lived and died';
   } else if (language === 'BE') {
-    name = 'Васiль';
+    name = currentAuthorObj.nameBY;
     YearsOflife = '1567г-1974г';
     ShortBiography = 'Нарадзіўся, жыў і памёр';
   }
