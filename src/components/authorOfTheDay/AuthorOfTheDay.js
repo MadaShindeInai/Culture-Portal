8e1462fd76eab2c79008b814b872873e226f4399
MadaShindeInai <1992@sergei.by>
2020-02-26 13:41:57 +0300

    feat: add basic appDescription and authorOfTheDay markup

--- src/components/authorOfTheDay/AuthorOfTheDay.js
+++ src/components/authorOfTheDay/AuthorOfTheDay.js
@@ -1,32 +1,42 @@
 import React from 'react';
 import './AuthorOfTheDay.scss';
+import Photo from '../../assets/images/bird.jpg'
 
 const AuthorOfTheDayRU = ({ language }) => {
 
   let name;
-  let authorPhoto;
-  let YearsOflife;
-  let ShortBiography;
+  let yearsOflife;
+  let shortBiography;
+  let authorOfTheDayTitle;
   if (language === 'RU') {
     name = 'Вася';
-    YearsOflife = '1567г-1974г';
-    ShortBiography = 'Родился, жил, умер';
+    yearsOflife = '1567г-1974г';
+    shortBiography = 'Родился, жил, умер';
+    authorOfTheDayTitle = 'Архитектор Дня';
   } else if (language === 'EN') {
     name = 'Vasya';
-    YearsOflife = '1567-1974';
-    ShortBiography = 'Born, lived and died';
+    yearsOflife = '1567-1974';
+    shortBiography = 'Born, lived and died';
+    authorOfTheDayTitle = 'Architect of the Day';
   } else if (language === 'BE') {
     name = 'Васiль';
-    YearsOflife = '1567г-1974г';
-    ShortBiography = 'Нарадзіўся, жыў і памёр';
+    yearsOflife = '1567г-1974г';
+    shortBiography = 'Нарадзіўся, жыў і памёр';
+    authorOfTheDayTitle = 'Архітэктар Дня';
   }
   return (
-    <div>
-      <img src={authorPhoto} alt="author" />
-      <div>
-        <p>{name}</p>
-        <p>{YearsOflife}</p>
-        <p>{ShortBiography}</p>
+    <div className="author-of-the-day">
+      <div className="author-of-the-day__wrapper">
+        <h2 className="author-of-the-day__title">{authorOfTheDayTitle}</h2>
+        <p className="decor"><span className="deg1" /><span className="deg2" /><span className="deg3" /></p>
+        <div className="author-of-the-day__content">
+          <img src={Photo} alt="author" width="250" />
+          <div className="author-of-the-day__description">
+            <p>{name}</p>
+            <p>{yearsOflife}</p>
+            <p>{shortBiography}</p>
+          </div>
+        </div>
 
       </div>
 
