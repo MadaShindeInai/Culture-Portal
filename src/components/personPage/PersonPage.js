0a04a7dae1a7a37bc54ed7467f24668d2b71edad
Deeamon <marinasugutova@gmail.com>
2020-02-27 22:23:59 +0300

    refactor: change map for language

--- src/components/personPage/PersonPage.js
+++ src/components/personPage/PersonPage.js
@@ -4,37 +4,26 @@ import Architects from '../../utils/architectsData';
 import './PersonPage.scss';
 
 const PersonPage = ({ language, id }) => {
-  const currentAuthor = Architects.filter((person) => person.path === id);
-  const currentAuthorObj = currentAuthor[0];
+  const currentAuthor = Architects.find(person => person.path === id);
 
-  let name;
-  let architectPhoto;
-  let date;
-  let description;
-  if (language === 'RU') {
-    name = currentAuthorObj.nameRU;
-    architectPhoto = currentAuthorObj.img;
-    date = currentAuthorObj.date;
-    description = currentAuthorObj.descriptionRU;
-  } else if (language === 'EN') {
-    name = currentAuthorObj.nameEN;
-    architectPhoto = currentAuthorObj.img;
-    date = currentAuthorObj.date;
-    description = currentAuthorObj.descriptionEN;
-  } else if (language === 'BE') {
-    name = currentAuthorObj.nameBY;
-    architectPhoto = currentAuthorObj.img;
-    date = currentAuthorObj.date;
-    description = currentAuthorObj.descriptionBY;
-  }
+  const lang = language === 'BE' ? 'BY' : language;
+
+  const projectTitles = {
+    date: { BY: 'Дата', RU: 'Дата', EN: 'Date' },
+    text: { BY: 'Апiсанне', RU: 'Описание', EN: 'Description' }
+  };
   return (
     <div className="info-container">
       <div className="info-container__item">
         <h2 className="info-container__name">{name}</h2>
       </div>
       <div className="info-container__item">
-        <img className="info-container__image"
-          src={architectPhoto} alt="photo" width="250" />
+        <img
+          className="info-container__image"
+          src={architectPhoto}
+          alt="photo"
+          width="250"
+        />
       </div>
       <div className="info-container__item">
         <h4 className="info-container__date">{date}</h4>
@@ -48,7 +37,7 @@ const PersonPage = ({ language, id }) => {
       <div className="info-container__item">Видео</div>
       <div className="info-container__item">Карта</div>
     </div>
-  )
-}
+  );
+};
 
 export default PersonPage;
