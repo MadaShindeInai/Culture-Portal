814f8c7ec27d1880813b49af065ca5032d91a179
Deeamon <marinasugutova@gmail.com>
2020-02-28 20:15:20 +0300

    feat: add modal window for video and fix fonts

--- src/components/personPage/PersonPage.js
+++ src/components/personPage/PersonPage.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import { Timeline, Table, Carousel } from 'antd';
+import React, { useState } from 'react';
+import { Timeline, Table, Carousel, Modal } from 'antd';
+import { YoutubeOutlined } from '@ant-design/icons';
 import { Map, Marker, TileLayer } from 'react-leaflet';
 import Architects from '../../utils/architectsData';
 
 import './PersonPage.scss';
 
 const PersonPage = ({ language, id }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const currentAuthor = Architects.find(person => person.path === id);
 
   const lang = language === 'BE' ? 'BY' : language;
@@ -67,13 +70,6 @@ const PersonPage = ({ language, id }) => {
           </Timeline>
         </div>
       </div>
-      <div className="info-container__item info-container__info-table">
-        <Table
-          dataSource={projects}
-          columns={projectColumns}
-          pagination={false}
-        />
-      </div>
       <div className="info-container__item info-container__info-carousel">
         <Carousel autoplay>
           {currentAuthor.projects.map(item => (
@@ -83,17 +79,40 @@ const PersonPage = ({ language, id }) => {
           ))}
         </Carousel>
       </div>
+      <div className="info-container__item info-container__info-table">
+        <Table
+          dataSource={projects}
+          columns={projectColumns}
+          pagination={false}
+        />
+      </div>
 
       {currentAuthor.videoId && (
         <div className="info-container__item">
-          <iframe
-            width="560"
-            height="315"
-            src={currentAuthor.videoId}
-            frameBorder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          />
+          <div>
+            <button
+              className="info-container__video-button"
+              type="button"
+              onClick={() => setIsModalOpen(true)}
+            >
+              <YoutubeOutlined />
+            </button>
+
+            <Modal
+              visible={isModalOpen}
+              closable={false}
+              onCancel={() => setIsModalOpen(false)}
+            >
+              <iframe
+                width="560"
+                height="315"
+                src={currentAuthor.videoId}
+                frameBorder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              />
+            </Modal>
+          </div>
         </div>
       )}
 
