3224bd1b1e300c57020bfb98eb03cc023accf974
Deeamon <marinasugutova@gmail.com>
2020-02-28 20:14:58 +0300

    fix: change order styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
 import 'antd/dist/antd.css';
+import App from './components/app';
 
 ReactDOM.render(<App />, document.getElementById('root'));
