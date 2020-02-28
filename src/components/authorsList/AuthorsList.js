4716f07aa67f9e303a6e244b6ff2fc62653fbf07
Darya Sidarovich <daria.sidorovich333@gmail.com>
2020-02-28 23:42:44 +0300

    fix: fix AuthorsList component

--- src/components/authorsList/AuthorsList.js
+++ src/components/authorsList/AuthorsList.js
@@ -9,21 +9,15 @@ const AuthorsList = ({ language, architects }) => {
       <ul>
         {architects.map((person) => {
           const url = `/authors/${person.path}`;
-          return <li
-            key={person.id}
-          ><Link to={url}
-            className='architects-list__item'
-          >{language === 'RU'
-            ? person.nameRU
-            : language === 'EN'
-              ? person.nameEN
-              : person.nameBY
-              }
+          return <li key={person.id}>
+            <Link to={url} className='architects-list__item'>
+              {language === 'RU' && person.nameRU
+                || language === 'EN' && person.nameEN
+                || person.nameBY}
               <p className="decor"><span className="deg1" /><span className="deg2" /><span className="deg3" /></p>
             </Link></li>
         })}
-
-      </ul >
+      </ul>
     </div>
   )
 }
