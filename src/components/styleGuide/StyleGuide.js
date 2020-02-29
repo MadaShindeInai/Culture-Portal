776d48a47065145e9a6a8dc8ffbd8b3ad02ed296
anastasiyaNasevichLeverx <anastasiya.nasevich@leverx.com>
2020-02-29 16:41:44 +0300

    fix: button styles

--- src/components/styleGuide/StyleGuide.js
+++ src/components/styleGuide/StyleGuide.js
@@ -5,20 +5,29 @@ import './StyleGuide.scss';
 const StyleGuide = ({ language }) => {
   let buttonColorText;
   let fontText;
+  let colorText;
+  let margin;
   if (language === 'RU') {
     buttonColorText = 'Кликни по кнопке';
-    fontText = 'Использовался шрифт'
+    fontText = 'Использовался шрифт';
+    colorText = 'Главный цвет';
+    margin = 'Все верхние отступы';
   } else if (language === 'EN') {
     buttonColorText = 'Click to Button';
     fontText = 'Our font - family style';
+    colorText = 'Main color';
+    margin = 'All margin - bottoms of projects';
+
   } else if (language === 'BE') {
     buttonColorText = 'Клікні па кнопцы';
     fontText = 'Выкарыстоўваўся шрыфт';
+    colorText = 'Галоўны колер';
+    margin = 'Увесь запас - дні праектаў';
   }
   return (
     <div className='style-container'>
       <div className='style-btn'>
-        <button>
+        <button className='style-btn_neon'>
           <span></span>
           <span></span>
           <span></span>
@@ -28,7 +37,7 @@ const StyleGuide = ({ language }) => {
       </div>
 
       <div className='style-btn'>
-        <button>
+      <button className='style-btn_neon'>
           <span></span>
           <span></span>
           <span></span>
@@ -39,6 +48,14 @@ const StyleGuide = ({ language }) => {
 
       <div className='style-font'>
         {fontText}: Futura MediumC
+      </div>
+
+      <div className='style-font'>
+        {colorText}:  #17a2b8;
+      </div>
+
+      <div className='style-font'>
+        {colorText}: 25px;
       </div>
     </div>
   )
