46940da4bd7bef033fca0f63418782fa93322d65
anastasiyaNasevichLeverx <anastasiya.nasevich@leverx.com>
2020-02-29 16:58:01 +0300

    feat: add points about libraries and api

--- src/components/styleGuide/StyleGuide.js
+++ src/components/styleGuide/StyleGuide.js
@@ -7,22 +7,26 @@ const StyleGuide = ({ language }) => {
   let fontText;
   let colorText;
   let margin;
+  let used;
   if (language === 'RU') {
     buttonColorText = 'Кликни по кнопке';
     fontText = 'Использовался шрифт';
     colorText = 'Главный цвет';
     margin = 'Все верхние отступы';
+    used = 'Использовались'
   } else if (language === 'EN') {
     buttonColorText = 'Click to Button';
     fontText = 'Our font - family style';
     colorText = 'Main color';
     margin = 'All margin - bottoms of projects';
-
+    used = 'Was used'
   } else if (language === 'BE') {
     buttonColorText = 'Клікні па кнопцы';
     fontText = 'Выкарыстоўваўся шрыфт';
     colorText = 'Галоўны колер';
     margin = 'Увесь запас - дні праектаў';
+    used = 'Выкарысталіся'
+
   }
   return (
     <div className='style-container'>
@@ -51,11 +55,21 @@ const StyleGuide = ({ language }) => {
       </div>
 
       <div className='style-font'>
-        {colorText}:  #17a2b8;
+        {colorText}:  #17a2b8
       </div>
 
       <div className='style-font'>
         {margin}: 25px;
+      </div>
+
+      <div className='style-font'>
+        {used}: React-Bootstrap
+      </div>
+      <div className='style-font'>
+        {used}: React-Leaflet
+      </div>
+      <div className='style-font'>
+        {used}: Google Map
       </div>
     </div>
   )
