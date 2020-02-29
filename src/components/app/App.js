23f17f525246e50c8d4479b7893870841ee0102e
MadaShindeInai <1992@sergei.by>
2020-02-29 17:07:55 +0300

    feat: add reverse snow animation

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -19,6 +19,12 @@ function App() {
 
   return (
     <div className="wrapper">
+      <div className="animation-wrapper">
+        <div className="particle particle-1" />
+        <div className="particle particle-2" />
+        <div className="particle particle-3" />
+        <div className="particle particle-4" />
+      </div>
       <Router>
         <Header language={language} setLanguage={setLanguage} />
         <main className="main">
